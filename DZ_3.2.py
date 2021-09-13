a97b4d68d8be017c836c4fb7be5d888b6057579d
Bohodir1982 <bh007@bk.ru>
2021-09-13 12:53:06 +0500

    hw3

---
+++ DZ_3.2.py
@@ -0,0 +1,5 @@
+def my_func(name, surname, year, city, email, telephone):
+    return ' '.join([name, surname, year, city, email, telephone])
+
+print(my_func(name='Bohodir', surname='Khusainov', year='1982', city='Tashkent', email='bh007@bk.ru',
+              telephone='+99890-324-88-22'))
---
+++ DZ_3.6.py
@@ -0,0 +1,4 @@
+def func(a):
+    return a.title()
+
+print(func("bohodir khusainov"))
